a904a1421e89442b14ad629aa4f0995fe78c5886
liuyixin-louis <zxcxzcz@qq.com>
2022-06-03 00:35:35 +0800

    kl

--- network/__init__.py
+++ network/__init__.py
@@ -1,4 +1,4 @@
-from network.mobilnetv2 import mobilenetv2
+from network.mobilenetv2 import mobilenetv2
 from network.vgg import vgg11, vgg11_bn, vgg13, vgg13_bn, vgg16, vgg16_bn
 from network.resnet import resnet18, resnet34, resnet50,resnet101 ,resnet152
 from network.densenet import densenet40_f12, densenet100_f12, densenet100_f24
